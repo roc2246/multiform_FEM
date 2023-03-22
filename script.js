b5412d46252257ce23380f275f8be7c47624b37b
Riley <riley.childs@yahoo.com>
2023-03-22 19:11:53 -0400

    Made buttons interactive based on step number

--- script.js
+++ script.js
@@ -4,15 +4,34 @@ const steps = document.getElementsByClassName("step");
 
 const nextStep = document.getElementsByClassName("btn--next-step")[0];
 const prevStep = document.getElementsByClassName("btn--go-back")[0];
+const confirmBtn = document.getElementsByClassName("btn--confirmBtn")[0]
+
+function hideSteps () {
+    Object.keys(steps).forEach((step) => {
+        steps[step].style.display = "none";
+      });
+}
+
+function btnMgmt () {
+    if (stepNo === steps.length - 2) {
+        nextStep.style.display = "none"
+        confirmBtn.style.display = "inline-block"
+    } else {
+        nextStep.style.display = "inline-block"
+        confirmBtn.style.display = "none"
+    }
+
+    if(stepNo === 0) prevStep.style.display = "none"
+}
 
 function stepIncrement(inc) {
-  Object.keys(steps).forEach((step) => {
-    steps[step].style.display = "none";
-  });
+  hideSteps()
   stepNo += inc;
+  prevStep.style.display = "inline-block"
   if (stepNo === steps.length) stepNo = 0;
-  if (stepNo < 0) stepNo = steps.length - 1;
+  if (stepNo < 0) prevStep.style.display = "none";
   steps[stepNo].style.display = "block";
+  btnMgmt()
 }
 
 nextStep.onclick = () => {
@@ -22,3 +41,11 @@ nextStep.onclick = () => {
 prevStep.onclick = () => {
   stepIncrement(-1);
 };
+
+confirmBtn.onclick= () =>{
+    hideSteps()
+    steps[steps.length - 1].style.display = "flex"
+    nextStep.style.display = "none"
+    prevStep.style.display = "none"
+    confirmBtn.style.display = "none"
+}
