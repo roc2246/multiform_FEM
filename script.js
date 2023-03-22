f2c623c530fcdb768aed98fcebfd4e7bef7f5557
Riley <riley.childs@yahoo.com>
2023-03-22 19:22:00 -0400

    Refactored btnMgmt() and stepIncrement()

--- script.js
+++ script.js
@@ -4,34 +4,37 @@ const steps = document.getElementsByClassName("step");
 
 const nextStep = document.getElementsByClassName("btn--next-step")[0];
 const prevStep = document.getElementsByClassName("btn--go-back")[0];
-const confirmBtn = document.getElementsByClassName("btn--confirm")[0]
+const confirmBtn = document.getElementsByClassName("btn--confirm")[0];
 
-function hideSteps () {
-    Object.keys(steps).forEach((step) => {
-        steps[step].style.display = "none";
-      });
+function hideSteps() {
+  Object.keys(steps).forEach((step) => {
+    steps[step].style.display = "none";
+  });
 }
 
-function btnMgmt () {
-    if (stepNo === steps.length - 2) {
-        nextStep.style.display = "none"
-        confirmBtn.style.display = "inline-block"
-    } else {
-        nextStep.style.display = "inline-block"
-        confirmBtn.style.display = "none"
-    }
+function btnMgmt() {
+  if (stepNo === steps.length - 2) {
+    nextStep.style.display = "none";
+    confirmBtn.style.display = "inline-block";
+  } else {
+    nextStep.style.display = "inline-block";
+    confirmBtn.style.display = "none";
+  }
 
-    if(stepNo === 0) prevStep.style.display = "none"
+  if (stepNo === 0) {
+    prevStep.style.display = "none";
+  } else {
+    prevStep.style.display = "inline-block";
+  }
+
+  if (stepNo === steps.length) stepNo = 0;
 }
 
 function stepIncrement(inc) {
-  hideSteps()
+  hideSteps();
   stepNo += inc;
-  prevStep.style.display = "inline-block"
-  if (stepNo === steps.length) stepNo = 0;
-  if (stepNo < 0) prevStep.style.display = "none";
   steps[stepNo].style.display = "block";
-  btnMgmt()
+  btnMgmt();
 }
 
 nextStep.onclick = () => {
@@ -42,10 +45,10 @@ prevStep.onclick = () => {
   stepIncrement(-1);
 };
 
-confirmBtn.onclick= () =>{
-    hideSteps()
-    steps[steps.length - 1].style.display = "flex"
-    nextStep.style.display = "none"
-    prevStep.style.display = "none"
-    confirmBtn.style.display = "none"
-}
+confirmBtn.onclick = () => {
+  hideSteps();
+  steps[steps.length - 1].style.display = "flex";
+  nextStep.style.display = "none";
+  prevStep.style.display = "none";
+  confirmBtn.style.display = "none";
+};
