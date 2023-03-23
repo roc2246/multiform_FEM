79f644f730e3d23565366c348e4a24057b292fcf
Riley <riley.childs@yahoo.com>
2023-03-23 14:15:18 -0400

    Created form validation for Personal Info

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ const nextStep = document.getElementsByClassName("btn--next-step")[0];
 const prevStep = document.getElementsByClassName("btn--go-back")[0];
 const confirmBtn = document.getElementsByClassName("btn--confirm")[0];
 
+const nameInput = document.getElementsByClassName(
+  "personal-info__controls--input"
+)[0];
+const emailInput = document.getElementsByClassName(
+  "personal-info__controls--input"
+)[1];
+const phoneInput = document.getElementsByClassName(
+  "personal-info__controls--input"
+)[2];
+
+const checkName = /^(?!\s*$).+/;
+const checkEmail =
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const checkPhone = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+
 function hideSteps() {
   Object.keys(steps).forEach((step) => {
     steps[step].style.display = "none";
@@ -26,14 +41,45 @@ function btnMgmt() {
   } else {
     prevStep.style.display = "inline-block";
   }
-
 }
 
+function verify(regex, input) {
+  if (!regex.test(input.value)) {
+    input.value = ""
+    input.focus();
+  }
+}
+
+let formatPhoneNumber = (str) => {
+  //Filter only numbers from the input
+  let cleaned = ('' + str).replace(/\D/g, '');
+  
+  //Check if the input is of correct length
+  let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+  };
+
+  return null
+};
+
+
 function stepIncrement(inc) {
-  hideSteps();
-  stepNo += inc;
-  steps[stepNo].style.display = "block";
-  btnMgmt();
+  if (
+    checkName.test(nameInput.value) &&
+    checkEmail.test(emailInput.value) &&
+    checkPhone.test(phoneInput.value)
+  ) {
+    hideSteps();
+    stepNo += inc;
+    steps[stepNo].style.display = "block";
+    btnMgmt();
+  } 
+
+  verify(checkName, nameInput)
+  verify(checkEmail, emailInput)
+  verify(checkPhone, phoneInput)
 }
 
 nextStep.onclick = () => {
