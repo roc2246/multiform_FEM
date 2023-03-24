a192f1d110cb59f476a42dec989647f6efc48f04
Riley <riley.childs@yahoo.com>
2023-03-24 12:40:36 -0400

    Created onclick border change for plan select

--- script.js
+++ script.js
@@ -6,19 +6,32 @@ const nextStep = document.getElementsByClassName("btn--next-step")[0];
 const prevStep = document.getElementsByClassName("btn--go-back")[0];
 const confirmBtn = document.getElementsByClassName("btn--confirm")[0];
 
-const nameInput = document.getElementById("name")
-const emailInput = document.getElementById("email")
-const phoneInput = document.getElementById("phone")
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
 
 const checkName = /^(?!\s*$).+/;
 const checkEmail =
   /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const checkPhone = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
 
+const plans = document.getElementsByClassName("plan__option");
+
 function hideSteps() {
   Object.keys(steps).forEach((step) => {
     steps[step].style.display = "none";
   });
+}
+
+function removeBorder() {
+  Object.keys(plans).forEach((plan) => {
+    plans[plan].style.borderStyle = "none";
+  });
+}
+
+function setBorderColor(no){
+  plans[no].style.borderStyle = "solid"
+  plans[no].style.borderColor = "black"
 }
 
 function btnMgmt() {
@@ -89,3 +102,13 @@ confirmBtn.onclick = () => {
   prevStep.style.display = "none";
   confirmBtn.style.display = "none";
 };
+
+
+Object.keys(plans).forEach((plan => {
+  plans[plan].onclick = () => {
+    removeBorder();
+    setBorderColor(plan)
+  };
+}))
+
+
